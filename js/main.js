128e8219e622c148abd7525fab32c1f672e40adc
Doron Shiffer-Sebba <shdoron1@gmail.com>
2015-05-28 15:04:15 -0400

    translations the way I like them

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(".maintranslation").click(function(){
-            $(".lastAction").slideToggle();
-            $( ".test" ).appendTo( ".col-md-8" );
+            $(this).next().children(".lastAction").slideToggle();	
     });
 
 $(".form-control").change(function() {
 			alert("Input value: " + $(".form-control").val());
 });
+
+function addNewBill()  {
+	var billInfo = ""  //get bill info
+	var legislator = 'Senator Cookie' //parse
+	var billText = "cookies mandatory at every restaurant"
+	var billLog = []
+	var bill = "" // html bill skeleton
+	$("footer").before("put it all together here")
+}
